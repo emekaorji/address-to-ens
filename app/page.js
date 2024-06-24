b0e8dd1c8edf34ea690e00f450da5a71eaa1396b
Emeka Orji <emekapraiseo@gmail.com>
2024-06-24 13:20:03 +0100

    Add reverse method

--- app/page.js
+++ app/page.js
@@ -1,12 +1,13 @@
 'use client';
 
-import { ethers, InfuraProvider, JsonRpcProvider } from 'ethers';
+import { JsonRpcProvider } from 'ethers';
 import { useCallback, useState } from 'react';
 
 export default function Home() {
   const [coinAddress, setCoinAddress] = useState('');
+  const [ensDomain, setEnsDomain] = useState('');
 
-  const getENSName = useCallback(async () => {
+  const getENSFromAddressName = useCallback(async () => {
     // Connect to the Ethereum network (Mainnet)
     const provider = new JsonRpcProvider(
       'https://mainnet.infura.io/v3/c528dc5a0a724b3696a61b876bab146f'
@@ -16,36 +17,91 @@ export default function Home() {
       // Lookup the ENS name
       const ensName = await provider.lookupAddress(coinAddress);
       if (ensName) {
-        console.log(`ENS Name: ${ensName}`);
+        alert(`ENS Name: ${ensName}`);
       } else {
-        console.log('No ENS name found for this address.');
+        alert('No ENS name found for this address.');
       }
     } catch (error) {
-      console.error(`Error: ${error}`);
+      alert(`Error: ${error}`);
     }
   }, [coinAddress]);
 
-  // Replace with the contract address of the deployed coin
-  // const contractAddress = "0x1234567890abcdef1234567890abcdef12345678";
+  const getAddressFromENS = useCallback(async () => {
+    // Connect to the Ethereum network (Mainnet)
+    const provider = new JsonRpcProvider(
+      'https://mainnet.infura.io/v3/c528dc5a0a724b3696a61b876bab146f'
+    );
 
-  const handleSubmit = useCallback(
+    try {
+      // Lookup the ENS name
+      const ensName = await provider.resolveName(ensDomain);
+      if (ensName) {
+        alert(`ENS Name: ${ensName}`);
+      } else {
+        alert('No ENS name found for this address.');
+      }
+    } catch (error) {
+      alert(`Error: ${error}`);
+    }
+  }, [ensDomain]);
+
+  const handleSubmit1 = useCallback(
     (event) => {
       event.preventDefault();
-      getENSName();
+      getENSFromAddressName();
     },
-    [getENSName]
+    [getENSFromAddressName]
+  );
+
+  const handleSubmit2 = useCallback(
+    (event) => {
+      event.preventDefault();
+      getAddressFromENS();
+    },
+    [getAddressFromENS]
   );
 
   return (
-    <main className='flex min-h-screen flex-col items-center justify-between p-24'>
-      <form onSubmit={handleSubmit}>
+    <main className='min-h-screen items-center justify-between p-24 text-center'>
+      <h2 className='text-center text-4xl'>Get ENS from Address</h2>
+      <form
+        className='flex flex-col items-center gap-2'
+        onSubmit={handleSubmit1}>
         <input
           type='text'
           value={coinAddress}
           onChange={(e) => setCoinAddress(e.target.value)}
         />
-        <button type='submit'>Get ENS Domain</button>
+        <button className='bg-slate-300 p-2' type='submit'>
+          Get ENS Domain
+        </button>
+      </form>
+      <br />
+      <br />
+      <h2 className='text-center text-4xl'>Get Address from ENS</h2>
+      <form
+        className='flex flex-col items-center gap-2'
+        onSubmit={handleSubmit2}>
+        <input
+          type='text'
+          value={ensDomain}
+          onChange={(e) => setEnsDomain(e.target.value)}
+        />
+        <button className='bg-slate-300 p-2' type='submit'>
+          Get Address
+        </button>
       </form>
     </main>
   );
 }
+
+/**
+ * Sample Domains and Addresses
+ *
+ * OpenSea - 0x5b3256965e7C3cF26E11FCAf296DfC8807C01073
+ * UniSwap - 0x1a9C8182C09F50C8318d769245beA52c32BE35BC
+ * Metamask - 0x0c54FcCd2e384b4BB6f2E405Bf5Cbc15a017AaFb
+ * MakerDAO - 0xfb3Ca875955675d091e6f82038A288e97284400f
+ *
+ * Vitalik - 0xd8dA6BF26964aF9D7eEd9e03E53415D37aA96045
+ */
